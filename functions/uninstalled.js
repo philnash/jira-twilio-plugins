14dee049b03144da405e4a8dc82b3cab3724c248
Phil Nash <philnash@gmail.com>
2020-06-02 16:38:19 +1000

    Protects uninstalled endpoint with JWT

--- functions/uninstalled.js
+++ functions/uninstalled.js
@@ -1,11 +1,17 @@
 const utils = require(Runtime.getAssets()["/utils.js"].path);
 
 exports.handler = async function (context, event, callback) {
-  const settings = await utils.findOrCreateDocument(
-    context,
-    utils.settingsKey(event.clientKey)
-  );
-  await utils.deleteDocument(context, settings.phoneNumber);
-  await utils.deleteDocument(context, utils.settingsKey(event.clientKey));
-  callback(null, "");
+  try {
+    const claims = await utils.decode(event.jwt, context);
+    const clientKey = claims.iss;
+    const settings = await utils.findOrCreateDocument(
+      context,
+      utils.settingsKey(clientKey)
+    );
+    await utils.deleteDocument(context, settings.phoneNumber);
+    await utils.deleteDocument(context, utils.settingsKey(clientKey));
+    callback(null, "");
+  } catch (error) {
+    callback(error);
+  }
 };
